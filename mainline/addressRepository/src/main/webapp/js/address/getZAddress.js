fd781f9c0628358bf2889fbf5487767c1f3312bf
yinyu <yinyu@localhost>
2014-09-13 08:50:07 +0000

    【地址库】ztree获取数据 【尹玉】

--- mainline/addressRepository/src/main/webapp/js/address/getZAddress.js
+++ mainline/addressRepository/src/main/webapp/js/address/getZAddress.js
@@ -1,4 +1,8 @@
 var setting = {
+		async: {
+			enable: true,
+			url: getUrl
+		},
 	data: {
 		simpleData: {
 			enable: true
@@ -6,23 +10,78 @@ var setting = {
 	},
 	callback: {
 		beforeClick: beforeClick,
-		onClick: onClick
+		onClick: onClick,
+		beforeExpand: beforeExpand,
+		onAsyncSuccess: onAsyncSuccess,
+		onAsyncError: onAsyncError
 	}
 };
 
 var addressId;
 var log, className = "dark";
+var zTree;
+var lastValue = "", nodeList = [], fontCss = {};
+
+
 function beforeClick(treeId, treeNode, clickFlag) {
 	className = (className === "dark" ? "":"dark");
 	return (treeNode.click != false);
 }
 function onClick(event, treeId, treeNode, clickFlag) {
 	addressId=treeNode.id;
-}		
+	alert(treeNode.level);
+}	
+
+function getUrl(treeId, treeNode) {
+	var url=cxt+"/address/getAddressTree?id="+treeNode.id;
+	return url;
+}
+
+function beforeExpand(treeId, treeNode) {
+	if (!treeNode.isAjaxing) {
+		treeNode.times = 1;
+		ajaxGetNodes(treeNode, "refresh");
+		return true;
+	} else {
+		alert("zTree 正在下载数据中，请稍后展开节点。。。");
+		return false;
+	}
+}
+function onAsyncSuccess(event, treeId, treeNode, msg) {
+	if (!msg || msg.length == 0) {
+		treeNode.icon = cxt+"/css/zTree/zTreeStyle/img/loading.gif";
+		return;
+	}
+	//每次最多加载100个
+	totalCount = 0;
+	if (treeNode.children.length < totalCount) {
+		setTimeout(function() {ajaxGetNodes(treeNode);}, perTime);
+	} else {
+		treeNode.icon = "";
+		zTree.updateNode(treeNode);
+		if(!treeNode.children[0]){
+			treeNode.icon = cxt+"/css/zTree/zTreeStyle/img/loading.gif";
+		}else{
+			
+			zTree.selectNode(treeNode.children[0]);
+		}
+	}
+}
+function onAsyncError(event, treeId, treeNode, XMLHttpRequest, textStatus, errorThrown) {
+	alert("异步获取数据出现异常。");
+	treeNode.icon = "";
+	zTree.updateNode(treeNode);
+}
+function ajaxGetNodes(treeNode, reloadType) {
+	if (reloadType == "refresh") {
+		treeNode.icon = "../../../css/zTreeStyle/img/loading.gif";
+		zTree.updateNode(treeNode);
+	}
+	zTree.reAsyncChildNodes(treeNode, reloadType, true);
+}
 
 
 function expandNode(e) {
-	var zTree = $.fn.zTree.getZTreeObj("tree"),
 	type = e.data.type,
 	nodes = zTree.getSelectedNodes();
 	if (type.indexOf("All")<0 && nodes.length == 0) {
@@ -54,25 +113,47 @@ function expandNode(e) {
 
 
 function searchTree(){
-	var treeObj = $.fn.zTree.getZTreeObj("tree");
+	treeObj= $.fn.zTree.getZTreeObj("tree");
+	var filterString = $("#searchA").val();
+	nodeList = treeObj.getNodesByParamFuzzy("name", filterString);
+	updateNodes(true);
 	var nodes = treeObj.getNodes();
+	
 	var cloneNodes = [];
 	var removeNodes=[];
 	$.each(nodes, function(index, node) {
 		cloneNodes.push(node);
 	});
-	var filterString = $("#searchA").val();
+	treeObj.showNodes(cloneNodes);
+	pushRemoveNodes(cloneNodes,removeNodes,filterString);
+	treeObj.hideNodes(removeNodes);
+}
+function updateNodes(highlight) {
+	var nodes = zTree.getNodes();
+	zTree.hideNodes(nodes);
+	
+	for( var i=0, l=nodeList.length; i<l; i++) {
+		nodeList[i].highlight = highlight;
+		zTree.updateNode(nodeList[i]);
+	}
+	zTree.showNodes(nodeList);
+}
+function pushRemoveNodes(cloneNodes,removeNodes,filterString){
+	
 	$.each(cloneNodes, function(index, node) {
 		var childrens=node.children;
+		if(!childrens)return;
+		treeObj.showNodes(childrens);
 		if (node.name.indexOf(filterString) != -1) {
 			return;
 		}else{
 			var count=0;
 			$.each(childrens,function(ci,cn){
 				if(!cn)return;
+				//递归查询
+				pushRemoveNodes(childrens,removeNodes,filterString);
 				if(cn.name.indexOf(filterString)==-1){
 					removeNodes.push(cn);
-					
 					count++;
 				}
 			});
@@ -82,9 +163,6 @@ function searchTree(){
 		}
 		
 		
-	});
-	$.each(removeNodes,function(j,node){
-		treeObj.removeNode(node);
 	});
 }
 
@@ -104,6 +182,7 @@ function initOption(){
 			}
 		});
 }
+
 function getAll(){
 $.ajax({
 	 type: "POST",
@@ -111,7 +190,7 @@ $.ajax({
 		data:{isBind:true},
 		success:function(optionData){
 	        var t = $("#tree");
-	        t = $.fn.zTree.init(t, setting, optionData);
+	        zTree = $.fn.zTree.init(t, setting, optionData);
 			
 		}
 	});
