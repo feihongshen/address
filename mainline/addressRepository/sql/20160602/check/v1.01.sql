4c6c8a592f32a26719972ec7b0b476dd7c8b0055
vince.zhou <vince.zhou@vipshop.com>
2016-06-01 14:36:48 +0800

    plcs #none 新增检查脚本

--- mainline/addressRepository/sql/20160602/check/v1.01.sql
+++ mainline/addressRepository/sql/20160602/check/v1.01.sql
@@ -1,3 +1,9 @@
 -- 验证脚本
 select station_code from delivery_stations limit 10;
 select * from system_config where `name` = 'isOpenGisSearch';
+-- 此验证脚本如果查询出来的记录为空，需要手工插入一条记录
+select * from Scheduled_Tasks where fire_Time < now() and status = 0 and task_type = 'updateIndexTask';
+
+-- 如果上一步为空，需要插入该sql：
+insert into `scheduled_tasks` ( `TASK_TYPE`, `STATUS`, `REFERENCE_TYPE`, `FIRE_TIME`, `COMPLETED_TIME`, `CREATED_AT`, `TRY_COUNT`) 
+VALUES ( 'updateIndexTask', '0', 'addressId', NOW(), NULL,NOW(), '0');
