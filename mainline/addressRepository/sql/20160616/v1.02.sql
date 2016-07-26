031406d2380c8014959e12c016e79424aae431c8
yuxin.he <yuxin.he@vipshop.com>
2016-07-26 11:33:46 +0800

    区分DMPdb

--- mainline/addressRepository/sql/20160616/v1.02.sql
+++ mainline/addressRepository/sql/20160616/v1.02.sql
@@ -6,5 +6,5 @@ add COLUMN user_code varchar(32) NOT NULL DEFAULT '' COMMENT '小件员登录名
 ALTER TABLE deliverer_rules add COLUMN DELIVERY_STATION_ID INT(11) not NULL DEFAULT 0 COMMENT '配送站点id';
 
 
--- 更新配送员维护菜单为小件员关联维护
+-- 更新配送员维护菜单为小件员关联维护 -- DMP DB执行 
 update dmp40_function set functionurl='${addrUrl}/delivererStationRule/delivererStationRule?${addrUser}${addrPsw}' ,functionname ='小件员关联维护'  where functionname = '配送员关联维护';
